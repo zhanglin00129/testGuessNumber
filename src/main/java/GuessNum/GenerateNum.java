6602a9ddfd74d00eae2e3aa689ffdce4776d512e
Zhang Lin <lin.zhang@rea-group.com>
2015-06-14 23:11:57 +0800

    add GenerateNum

--- src/main/java/GuessNum/GenerateNum.java
+++ src/main/java/GuessNum/GenerateNum.java
@@ -1,7 +1,21 @@
 package GuessNum;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GenerateNum {
     public String generate() {
-        return null;
+        String result = "";
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++)
+            list.add(new Integer(i));
+        //打乱
+        Collections.shuffle(list);
+        for (int i = 0; i < 4; i++) {
+            result += (char) (list.get(i) + 48);
+        }
+
+        return result;
     }
 }
